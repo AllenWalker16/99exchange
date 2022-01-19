41daad51578afdc2f7cb5601c1d1ee1e5d95ba74
Allen <allenwalker1603@gmail.com>
2022-01-19 01:49:09 +0100

    good

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#DAFB52',
+        secondary: '#E1E5D6',
+        tertiary: '#F0F4E5',
+        final: '#FCFFF5',
+      },
+      fontFamily: {
+        sans: [
+          'Josefin Sans'
+        ],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
